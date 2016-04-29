ae8704a6ad83ca736e30f40c2cfced04f3049ba1
Sara JC Gosline <sara.gosline@sagebase.org>
2016-04-29 14:49:40 -0700

    Uploaded matrices to synapse, still need to annotate with gene name

--- analysis/2016-04-29/formatEncodeSkinData.R
+++ analysis/2016-04-29/formatEncodeSkinData.R
@@ -22,8 +22,11 @@ all.tabs<-lapply(synids,function(x) {
 nv<-which(unlist(lapply(all.tabs,is.null)))
 all.tabs<-all.tabs[-nv]
 tsvs<-tsvs[-nv]
+
+##get teh indcies of the tsvs
 hg19.tsvs<-which(metadata[match(tsvs,metadata$File.accession),ncol(metadata)-1]=='hg19')
 grch38.tsvs<-which(metadata[match(tsvs,metadata$File.accession),ncol(metadata)-1]=='GRCh38')
+
 
 hg19.tpm.mat<-sapply(all.tabs[hg19.tsvs],function(x){
   counts<-as.numeric(x$TPM)
@@ -55,12 +58,40 @@ grch38.fpkm.mat<-sapply(all.tabs[grch38.tsvs],function(x){
 colnames(grch38.tpm.mat)<-tsvs[grch38.tsvs]
 colnames(grch38.fpkm.mat)<-tsvs[grch38.tsvs]
 
+cf='https://raw.githubusercontent.com/sgosline/dNFLandscape/master/analysis/2016-04-29/formatEncodeSkinData.R'
 write.table(hg19.tpm.mat,file='hg19AlignedTPMs.csv',sep=',',quote=F)
 write.table(hg19.fpkm.mat,file='hg19AlignedFPKMs.csv',sep=',',quote=F)
 
 write.table(grch38.tpm.mat,file='grch38AlignedTPMs.csv',sep=',',quote=F)
 write.table(grch38.fpkm.mat,file='grch38AlignedFPKMs.csv',sep=',',quote=F)
 
-synStore(File('hg19AlignedTPMs.csv',parentId=''),used=list(url='',wasExecuted=TRUE))
+synStore(File('hg19AlignedTPMs.csv',parentId='syn6035836'),
+         executed=list(list(url=cf)),
+         used=lapply(synids[hg19.tsvs],function(x) list(entity=x)))
+synStore(File('hg19AlignedFPKMs.csv',parentId='syn6035836'),
+         executed=list(list(url=cf)),
+         used=lapply(synids[hg19.tsvs],function(x) list(entity=x)))
+
+synStore(File('grch38AlignedTPMs.csv',parentId='syn6035836'),
+         executed=list(list(url=cf)),
+         used=lapply(synids[grch38.tsvs],function(x) list(entity=x)))
+synStore(File('grch38AlignedFPKMs.csv',parentId='syn6035836'),
+         executed=list(list(url=cf)),
+         used=lapply(synids[grch38.tsvs],function(x) list(entity=x)))
+
+gene.names<-union(rownames(hg19.tpm.mat),rownames(grch38.tpm.mat))
+
+gene.base<-unique(sapply(gene.names, function(x) unlist(strsplit(x,split='.',fixed=T))[1]))
+#write.table(gene.names,quote=F,row.names=F,col.names=F,file='ensemblGeneNames.txt')
+
+require(biomaRt)
+ensembl=useMart("ENSEMBL_MART_ENSEMBL",dataset="hsapiens_gene_ensembl",host='www.ensembl.org')
+filters = listFilters(ensembl)
+attributes = listAttributes(ensembl)
+
+epep="ensembl_gene_id"
+egene='hgnc_symbol'
+gene.mapping<-getBM(attributes=c(epep,egene),filters=c(epep),values=gene.base,mart=ensembl)
+
 
 
