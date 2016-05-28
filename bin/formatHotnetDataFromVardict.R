41bdadbfccf4e7aa6efefe8bb0eaf83c5be8271f
Sara Gosline <sara.gosline@sagebase.org>
2016-05-28 03:07:14 +0000

    Tried to fix bug

--- bin/formatHotnetDataFromVardict.R
+++ bin/formatHotnetDataFromVardict.R
@@ -28,13 +28,14 @@ getCountsFromTable<-function(tab,mutationType=c('germline','somatic'),effect="HI
   #  tab <- tab[which(tab$Mutation_Type!='Silent'),]
   tab$Patient=sapply(tab$Sample,function(x) paste(unlist(strsplit(as.character(x),split='_'))[1:2],collapse='_'))
   #mut.idx = which(sapply(tab$Mutation_Status,tolower)%in%mutationType)
-  tab <- subset(tab,Effect%in%unlist(strsplit(effect,split='_'))&&Status%in%mutationType)
+  tab <- subset(tab,Effect%in%unlist(strsplit(effect,split='_')))
+  tab<- subset(tab,Status%in%mutationType)
   
  # tab<- tab[which(tab$Hugo_Symbol%in%expressed.genes),]
   if(sampOrPat=='sample')
     num.patients = tab %>% group_by(Gene) %>% summarize(Samples = n_distinct(Sample))
   else
-    num.patnets = tab%>% group_by(Gene)%>%summarize(Patients=n_distinct(Patient))
+    num.patients = tab%>% group_by(Gene)%>%summarize(Patients=n_distinct(Patient))
   
   fname= paste(prefix,paste(mutationType,collapse='_and_'),'mutationsWith',effect,'effectNum',sampOrPat,'perGene.tab',sep='')  
   write.table(num.patients,file=fname,col.names=F,row.names=F,quote=F)
@@ -45,8 +46,8 @@ files=c()
 for(mutType in c(c('Germline'),c("StrongSomatic","LikelySomatic"),c("StrongSomatic","LikelySomatic","StrongLOH","LikelyLOH"))){
 #mutType<-'germline'
  for(mt in c("HIGH","HIGH_MODERATE","HIGH_MODERATE_LOW")){
-      files=c(files,getCountsFromTable(expr.gene.muts05,mutationType=mutType,sampOrPat=ifelse(length(mutType)==1,'patient','sample'),prefix='p05'))
-      files=c(files,getCountsFromTable(expr.gene.muts1,mutationType=mutType,sampOrPat=ifelse(length(mutType)==1,'patient','sample'),prefix='p1'))
+      files=c(files,getCountsFromTable(expr.gene.muts05,mutationType=mutType,effect=mt,sampOrPat=ifelse(length(mutType)==1,'patient','sample'),prefix='p05'))
+      files=c(files,getCountsFromTable(expr.gene.muts1,mutationType=mutType,effect=mt,sampOrPat=ifelse(length(mutType)==1,'patient','sample'),prefix='p1'))
       
        }
   
