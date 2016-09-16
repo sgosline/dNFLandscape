c6b2a8a4e6d1c0aa62251ca4ee7b7e787da4004e
Sara JC Gosline <sara.gosline@sagebase.org>
2016-09-16 15:45:02 -0700

    updated with new cibersort plo

---
+++ analysis/2016-09-16/rePlotCiberSort.R
@@ -0,0 +1,36 @@
+##replot cibersort
+require(synapseClient)
+synapseLogin()
+
+cs.res<-read.table(synGet('syn5809355')@filePath,sep=',',header=T)
+
+##the PBK ids are missing from table, so need to query annotations
+res<-synQuery("select patientID,tissueID,sampleID from entity where parentId=='syn5492805'")
+#map<-unique(res)
+
+#from table get generic tumor id
+tres<-synTableQuery("SELECT Patient,RnaID,TumorNumber,'RNASeq (Cufflinks)' FROM syn5556216 where RnaID is not NULL")@values
+
+idx<-match(res$entity.id,tres$`RNASeq (Cufflinks)`)
+
+dres<-res[which(!is.na(idx)),]
+tres<-tres[idx[which(!is.na(idx))],]
+
+full.map<-cbind(dres,tres)
+
+#map tumors to sample ids
+sampleIds<-sapply(cs.res$Input.Sample,function(x){
+  y=which(full.map$entity.sampleID==gsub('”','',x))
+  paste("Patient",full.map$Patient[y],"Tumor",full.map$TumorNumber[y])
+  
+})
+
+rownames(cs.res)<-sampleIds
+cs.res<-cs.res[,-1]
+
+this.file=''
+library(pheatmap)
+pheatmap(cs.res[,1:22],annotation_row=cs.res[,23:25],cellheight=10,cellwidth = 10,filename='ciberSortRePlotted.png')
+synStore(File('ciberSortRePlotted.png',parentId='syn5809348'),executed=list(list(url=this.file)))
+
+
