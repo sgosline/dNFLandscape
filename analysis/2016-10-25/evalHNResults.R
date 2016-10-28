459c7f6b6481b5dbb9c2ab4f8dd9afdb57fb3638
Sara JC Gosline <sara.gosline@sagebase.org>
2016-10-28 13:54:29 -0700

    Updated with new hotnet results

--- analysis/2016-10-25/evalHNResults.R
+++ analysis/2016-10-25/evalHNResults.R
@@ -2,11 +2,13 @@ source("../../bin/wgsAnalysis.R")
 
 ##can we plot individual genes to view
 genes=c("MDC1","CDC27","NRP2","VEGFC","SEMA3B","FLT4","CREBBP")
-genes=c("PRKDC",'MAP3K14','VPS11','RRM1','RECQL4')
+genes=c("PRKDC",'MAP3K14','VPS11','RRM1','RECQL4',"CDC27","CREBBP")
+
 
 for(g in genes){
-  stats<-getMutationStatsForGene(expr.gene.muts1,gene=g,doPlot=TRUE,effect=c("MODERATE","HIGH"),prefix='p1')
+  stats<-getMutationStatsForGene(expr.gene.muts1,gene=g,doPlot=TRUE,effect=c("HIGH"),prefix='p1')
 
 }
 
 ##moderate impact mutations will be a tough sell.  can we find any high impact ones?  
+#we should probably store some oft hose images on synapse. 
