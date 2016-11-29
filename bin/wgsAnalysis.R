9dca10d719fea96fcc3cb95d6e93f51b2e8f0a1d
allaway <robert.allaway@sagebase.org>
2016-11-28 17:02:14 -0800

    minor changes

--- bin/wgsAnalysis.R
+++ bin/wgsAnalysis.R
@@ -1,7 +1,7 @@
 ###plotting of WGS variants
 
 source("../../dermalNF/bin/WGSData_VarDict.R")
-source('../../../dermalNF/bin/dermalNFData.R')
+source('../../dermalNF/bin/dermalNFData.R')
 cancer.genes<-read.csv('../../data/Census_allTue Jan 19 18-58-56 2016.csv')
 if(!exists('expr.gene.muts05'))
   expr.gene.muts05<-subset(read.table(synGet("syn6097853")@filePath,sep='\t'),PASS=='TRUE')
