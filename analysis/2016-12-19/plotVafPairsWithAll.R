ad21a490e738a701653a16ba65b3fa7c9a23c18d
Sara JC Gosline <sara.gosline@sagebase.org>
2016-12-20 10:12:04 -0500

    updated with correct code

--- analysis/2016-12-19/plotVafPairsWithAll.R
+++ analysis/2016-12-19/plotVafPairsWithAll.R
@@ -1,6 +1,7 @@
 source("../../bin/wgsAnalysis.R")
 library(ggplot2)
 library(tidyr)
+library(data.table)
 ##now take allele frequency for each pair for each patient
 this.script='https://raw.githubusercontent.com/sgosline/dNFLandscape/master/analysis/2016-12-19/plotVafPairsWithAll.R'
 
@@ -11,25 +12,29 @@ patients<-sapply(samples,function(x) unlist(strsplit(x,split='_'))[2])
 pfiles<-synapseQuery("select * from entity where parentId=='syn5605256'")
 
 getFileByPatient<-function(x){
-  pfiles[grep(paste('pval0.05patient',x,sep='_'),pfiles$entity.name),]$entity.id
+  pfiles[grep(paste('pval0.05patient',x,'impact',sep='_'),pfiles$entity.name),]$entity.id
   
 }
 plotVafByPairs<-function(sampMat,patient){
-  mutDf<-read.table(synGet(getFileByPatient(patient))@filePath,sep='\t')
+  mutDf<-as.data.table(fread(synGet(getFileByPatient(patient))@filePath,sep='\t'))
+  
     somDf<-subset(mutDf,Status%in%c('StrongSomatic','LikelySomatic'))#,'LikelyLOH'))
     somDf<-subset(somDf,!Chr%in%c('chrX','chrY'))
     pdf(paste('patient',patient,'varAlleleFreqBySamp.pdf',sep='_'))
+    sapply(unique(c(sampMat)),function(x){
+      plot(density(subset(somDf,Sample==x)$AlleleFreq),main=paste("Allele Frequency for",x))
+    })
     apply(sampMat,2,function(x){
         pairs<-subset(somDf,Sample%in%x)
         if(length(unique(pairs$Sample))==1)
           return(NULL)
         #res<-pairs%>%select(Sample,Gene,AlleleFreq)
-        res<-pairs%>%unite(Pos,Chr,Start,sep='_')%>%select(Sample,Gene,Pos,AlleleFreq,VD,RD)
+        res<-pairs%>%unite(Pos,Chr,Start,sep='_')%>%select(Sample,Gene,Pos,AlleleFreq,VD,RD,Status,Effect)
        # mafs<-unique(res)%>%spread(AlleleFreq,Sample,fill=0.0)
         amat<-acast(res,Pos~Sample,fill=0.0,value.var='AlleleFreq',fun.aggregate=mean)
-        newdf<-data.frame(amat,Gene=res$Gene[match(rownames(amat),res$Pos)])
-        gz<-intersect(which(apply(newdf[,1:2],1,function(x) all(x>0))),which(newdf$Gene!=''))
-        p<-ggplot(newdf,mapping=aes_string(x=x[1],y=x[2]))+geom_point()+geom_text(data=newdf[gz,],mapping=aes_string(x=x[1],y=x[2],label="Gene"))
+        newdf<-data.frame(amat,Gene=res$Gene[match(rownames(amat),res$Pos)],Status=res$Status[match(rownames(amat),res$Pos)],Effect=res$Effect[match(rownames(amat),res$Pos)])
+        gz<-intersect(which(apply(newdf[,1:2],1,function(x) all(x>0))),which(newdf$Effect!='MODIFIER'))
+        p<-ggplot(newdf,mapping=aes_string(x=x[1],y=x[2]))+geom_point(aes(color=Status))+geom_text(data=newdf[gz,],mapping=aes_string(x=x[1],y=x[2],label="Gene"))
         print(p)
           })
     dev.off()
