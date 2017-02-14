367e2b31d6b489f0d6e44296d813e2aa25c69e89
allaway <robert.allaway@sagebase.org>
2017-02-13 16:26:30 -0800

    updated

--- analysis/2017-02-13/discordantProteomicsFiles.R
+++ analysis/2017-02-13/discordantProteomicsFiles.R
@@ -10,4 +10,7 @@ join <- select(join, CTF_Sample_Description, Sample_No, GSL_Sample_ID, Sample.ID
 
 names(join) <- c("CTF_Sample_Description", "Sample_No", "New_ID", "Old_ID", "Sequencing_Library_ID", "Proteomics_Data", "Remark")
 
-synStore()
+write.table(join, file = "discordant_proteomics.txt", sep = "\t")
+
+this.file<-"https://raw.githubusercontent.com/allaway/dNFLandscape/200c93be385335f356fb5c9f0a42e882c70bdaf0/analysis/2017-02-13/discordantProteomicsFiles.R"
+synStore(File("discordant_proteomics.txt", parentId = "syn7899337"), used = c("syn4999547", "syn8256971"), executed = this.file)
